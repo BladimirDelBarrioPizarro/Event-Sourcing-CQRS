4b0a8445fdd04b3d65dc0a83056d2d1f095b561b
Bladi <bladimaltego@gmail.com>
2022-08-29 12:32:22 +0200

    feat: added class AccountEventStore in account.cmd.infraestructure

---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountEventStore.java
@@ -0,0 +1,58 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.banking.account.cmd.domain.AccountAggregate;
+import com.banking.account.cmd.domain.EventStoreRepository;
+import com.banking.cqrs.core.events.BaseEvent;
+import com.banking.cqrs.core.events.EventModel;
+import com.banking.cqrs.core.exceptions.AggregateNotFoundException;
+import com.banking.cqrs.core.exceptions.ConcurrencyException;
+import com.banking.cqrs.core.infrastructure.EventStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AccountEventStore implements EventStore {
+
+    @Autowired
+    private EventStoreRepository eventStoreRepository;
+
+    @Override
+    public void saveEvents(String aggregateId, Iterable<BaseEvent> baseEvents, int expectedVersion) {
+        var eventString = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        // Comprobamos si la versión que espero es válida
+        if(expectedVersion != -1 && eventString.get(eventString.size() -1).getVersion() != expectedVersion){
+            throw new ConcurrencyException();
+        }
+
+        var version = expectedVersion;
+        for(var event: baseEvents){
+            version++;
+            event.setVersion(version);
+            var eventModel = EventModel.builder()
+                    .timeStamp(new Timestamp(System.currentTimeMillis()))
+                    .aggregateIdentifier(aggregateId)
+                    .aggregateType(AccountAggregate.class.getTypeName())
+                    .version(version)
+                    .eventType(event.getClass().getTypeName())
+                    .baseEvent(event)
+                    .build();
+            var persistedEvent = eventStoreRepository.save(eventModel);
+            if(persistedEvent != null){
+                // Producir un evento para Kafka
+            }
+        }
+    }
+
+    @Override
+    public List<BaseEvent> getEvent(String aggregateId) {
+        var eventString = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if(eventString == null || eventString.isEmpty()){
+            throw new AggregateNotFoundException(" -- ERROR la cuenta dle banco es incorrecta");
+        }
+        return eventString.stream().map(EventModel::getBaseEvent).collect(Collectors.toList());
+    }
+}
--- cqrs-es/cqrs.core/src/main/java/com/banking/cqrs/core/events/EventModel.java
+++ cqrs-es/cqrs.core/src/main/java/com/banking/cqrs/core/events/EventModel.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Data
@@ -13,7 +14,7 @@ import java.util.Date;
 public class EventModel {
     @Id
     private String id;
-    private Date timeStamp;
+    private Timestamp timeStamp;
     private String aggregateIdentifier;
     private String aggregateType;
     private int version;
