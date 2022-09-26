5281bbf4c00c56cc00be7aa2df12ce5f61d95b52
Bladi <bladimaltego@gmail.com>
2022-09-26 13:36:27 +0200

    feat: added controllers

--- banking-account/account.common/src/main/java/com/banking/account/common/events/AccountClosedEvent.java
+++ banking-account/account.common/src/main/java/com/banking/account/common/events/AccountClosedEvent.java
@@ -3,10 +3,12 @@ package com.banking.account.common.events;
 import com.banking.cqrs.core.events.BaseEvent;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class AccountClosedEvent extends BaseEvent {
 }
